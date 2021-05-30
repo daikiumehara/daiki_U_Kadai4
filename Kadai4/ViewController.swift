7d2faa9049714a81bd3d0cfbec47e4922bee84d5
daiki_U <daikiumehara3@gmail.com>
2021-05-30 22:28:17 +0900

    first

---
+++ Kadai4/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Kadai4
+//
+//  Created by daiki umehara on 2021/05/30.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ViewController: UIViewController {
+    @IBOutlet private var incrementButton: UIButton!
+    @IBOutlet private var clearButton: UIButton!
+    @IBOutlet var label: UILabel!
+    
+    private var labelValue: Int = 0 {
+        didSet {
+            self.label.text = String(describing: self.labelValue)
+        }
+    }
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        didTapIncrementButton()
+        didTapClearButton()
+    }
+    private func didTapIncrementButton() {
+        incrementButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.labelValue += 1
+            })
+            .disposed(by: disposeBag)
+    }
+    private func didTapClearButton() {
+        clearButton.rx.tap
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.labelValue = 0
+            })
+            .disposed(by: disposeBag)
+    }
+}
